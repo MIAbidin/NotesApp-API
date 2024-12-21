728ff65c0d7b77d2a1b370181f2dedbe5121f366
MIAbidin <muhammadirfanabidin@gmail.com>
2024-12-21 12:16:00 +0700

    authorization implemented

--- src/server.js
+++ src/server.js
@@ -1,31 +1,31 @@
 // mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
- 
+
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
-const ClientError = require('./exceptions/ClientError');
 
 // notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
- 
+const ClientError = require('./exceptions/ClientError');
+
 // users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
- 
+
 // authentications
 const authentications = require('./api/authentications');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
- 
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
- 
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -35,14 +35,14 @@ const init = async () => {
       },
     },
   });
- 
+
   // registrasi plugin eksternal
   await server.register([
     {
       plugin: Jwt,
     },
   ]);
- 
+
   // mendefinisikan strategy autentikasi jwt
   server.auth.strategy('notesapp_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
@@ -59,7 +59,7 @@ const init = async () => {
       },
     }),
   });
- 
+
   await server.register([
     {
       plugin: notes,
@@ -102,9 +102,9 @@ const init = async () => {
 
     return h.continue;
   });
- 
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
